7fb418815c6eba13ad6fedc85eedcef9a0235147
zaydyaghi-lab <zaydyaghi@gmail.com>
2026-02-28 15:05:57 -0600

    Add script.js for Liquid Glass interactions

---
+++ web/script.js
@@ -0,0 +1,11 @@
+// JavaScript for web Liquid Glass interactions
+
+function initializeLiquidGlass() {
+    // Code to initialize Liquid Glass effects
+}
+
+function updateLiquidGlass() {
+    // Code to update Liquid Glass on interaction
+}
+
+initializeLiquidGlass();
